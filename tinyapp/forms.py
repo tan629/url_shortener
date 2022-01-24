5322fd2d86e34c0454b7edd3c3f15e15ea0eb0de
Tanimul Amin <tanimul.amin@versaterm.com>
2022-01-24 00:16:14 -0500

    -Created regitration form with selected fields from the built-in User model of Django

---
+++ tinyapp/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
