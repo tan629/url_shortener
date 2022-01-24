385fd67c7a4949c617c973f513957582ff4d071c
Tanimul Amin <tanimul.amin@versaterm.com>
2022-01-24 00:15:19 -0500

    -Created view for regitration page

--- tinyapp/views.py
+++ tinyapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import User
+from .forms import UserRegisterForm
 
 # Create your views here.
+class UserRegistrationView(CreateView):
+    form_class = UserRegisterForm
+    success_url = '/register'
+    template_name = 'register.html'
+
