a874976d04d5d919b3f88f0c98a4fef0b96b34ee
Tanimul Amin <tanimul.amin@versaterm.com>
2022-01-25 18:12:09 -0500

    -Changed field name 'user_id' to 'user' since django adds _id to foreignkeys automatically upon migration

--- tinyapp/models.py
+++ tinyapp/models.py
@@ -7,8 +7,8 @@ class User(AbstractUser):
 
 # Create your models here.
 class Url(models.Model):
-    short_url = models.CharField(max_length=50, unique=True)
-    long_url = models.CharField(max_length=50)
-    user_id = models.ForeignKey('User',on_delete=models.CASCADE,)
+    short_url = models.CharField(verbose_name='Short URL',max_length=50, unique=True)
+    long_url = models.CharField(verbose_name='Long URL',max_length=50)
+    user = models.ForeignKey('User',on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now=True)
     
