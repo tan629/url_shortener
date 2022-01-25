4f7b2dbe43fc4c87bb2e19de056ad1abaf8bee2a
Tanimul Amin <tanimul.amin@versaterm.com>
2022-01-25 18:18:09 -0500

    -Added a form class for new URL creation

--- tinyapp/forms.py
+++ tinyapp/forms.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from .models import User
+from django.forms import ModelForm,TextInput
+from .models import User,Url
 
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+    
+class UrlModelForm(ModelForm):
+    class Meta:
+        model = Url
+        fields = ['long_url']
+        widgets = {
+            'long_url': TextInput(attrs={'placeholder': 'http://'})
+        }
