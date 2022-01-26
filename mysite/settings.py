ff0f2be27f85c53c635365f910c41410c3cb2be3
Tanimul Amin <tanimul.amin@versaterm.com>
2022-01-26 13:17:43 -0500

    -Corrected the DB user environment variable name

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,7 @@ import environ
 
 # Initialise environment variables
 env = environ.Env()
+
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,7 +88,7 @@ DATABASES = {
 
         'NAME': env('DATABASE_NAME'),
 
-        'USER': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
 
         'PASSWORD': env('DATABASE_PASS'),
 
