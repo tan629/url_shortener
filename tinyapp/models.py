1697f4f150669327103607a0147155e517ebdc8d
Tanimul Amin <tanimul.amin@versaterm.com>
2022-01-28 18:25:05 -0500

    -Made Long URL field URLField type so that it is validated as a URL -Set max length of long url to 500 and short url to 6

--- tinyapp/models.py
+++ tinyapp/models.py
@@ -8,8 +8,8 @@ class User(AbstractUser):
 
 # Definition of URL model
 class Url(models.Model):
-    short_url = models.CharField(verbose_name='Short URL',max_length=50, unique=True)
-    long_url = models.CharField(verbose_name='Long URL',max_length=50)
+    short_url = models.CharField(verbose_name='Short URL', max_length=6,unique=True)
+    long_url = models.URLField(verbose_name='Long URL',max_length=500)
     user = models.ForeignKey('User',on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now=True)
     
