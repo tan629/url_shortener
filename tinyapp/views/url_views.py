6a1b397fcffa6985ec44d68bab63dccd4562643c
Tanimul Amin <tanimul.amin@versaterm.com>
2022-01-28 19:30:36 -0500

    -Corrected the logic to check authenticity of logged in user when trying to view a URL -Added comments to code

--- tinyapp/views/url_views.py
+++ tinyapp/views/url_views.py
@@ -14,6 +14,7 @@ class UrlListView (ListView):
 
     template_name = 'urls_index.html'
     
+    # Get coookie info from context data
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['username'] = self.request.session.get('username')
@@ -36,10 +37,12 @@ class UrlCreateView(CreateView):
     success_url = '/urls'
     template_name = 'urls_new.html'
     
+    #This method generates a short URL
     def get_short_url(self):
         letters = string.ascii_lowercase + string.digits + string.ascii_uppercase
         return ''.join(random.choice(letters) for i in range(6))
 
+    #Validate the create URL form
     def form_valid(self, form):
         
         current_user_id = self.request.user.id
@@ -48,7 +51,8 @@ class UrlCreateView(CreateView):
         form.instance.short_url = self.get_short_url()
 
         return super().form_valid(form) #If form fields are valid, it redirects to success_url defined in this class
-      
+    
+    # Get coookie info from context data
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['username'] = self.request.session.get('username') # Set cookie in the context object
@@ -79,6 +83,7 @@ class UrlUpdateView(UpdateView):
     template_name = 'url_detail.html' # template for updating URL
     success_url=reverse_lazy("url-list")  # Redirect to the urls list on success
     
+    # Get coookie info from context data
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['username'] = self.request.session.get('username') # Set cookie in the context object
@@ -90,8 +95,13 @@ class UrlUpdateView(UpdateView):
         
         logged_in_user = self.request.session.get('username')
         
-        logged_in_user_id = User.objects.filter(username=logged_in_user)[0].id
+        logged_in_user_id = None
         
+        if logged_in_user:
+            logged_in_user_id = User.objects.filter(username=logged_in_user)[0].id
+        
+        #Check if the user id of the URL object is same as the logged in user id
+        #If not, then deny access to the URL
         if(self.object.user_id != logged_in_user_id):
             return HttpResponseForbidden()
         
